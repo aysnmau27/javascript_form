c4937d16d16a886f0ac29ef340ce22d9d65a3dff
COMLAB <mayson23-0316@cca.edu.ph>
2025-02-19 18:40:36 +0800

    Create table.php

---
+++ table.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Table Form</title>
+    </head>
+    <body>
+
+    <h1>Student Record</h1>
+    <table id="student_record">
+        <!-- WALANG LAMAN -->
+    </table>
+
+    </body>
+</html>
